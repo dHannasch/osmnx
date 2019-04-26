937af8c4b58a1faabe76769bf90f602c1b9d0a42
David A. Hannasch <David.A.Hannasch@gmail.com>
2019-04-26 12:54:16 -0600

    create function fetch_elevations_for_latitude_longitude_pairs but do not use it yet (this commit exists only to have an easy-to-read diff)

--- osmnx/elevation.py
+++ osmnx/elevation.py
@@ -10,12 +10,96 @@ import math
 import time
 import requests
 import pandas as pd
+import numpy as np
 import networkx as nx
 
 from .core import save_to_cache
 from .core import get_from_cache
 from .utils import log
 
+
+def fetch_elevations_for_latitude_longitude_pairs(latitude_longitude_pairs,
+                                                  url_template_kwargs,
+                                                  decimal_places_to_round=5,
+                                                  max_locations_per_batch=350,
+                                                  pause_duration=0.02,
+                                                  url_template='https://maps.googleapis.com/maps/api/elevation/json?locations={locations}&key={api_key}',
+                                                  proxies=dict(),
+                                                  ):
+    """
+    Get the elevation (meters) of each point in the DataFrame and add it to the
+    row as an attribute.
+
+    Parameters
+    ----------
+    latitude_longitude_pairs : pd.DataFrame
+        the points to fetch elevation for;
+        this DataFrame must have columns 'latitude' and 'longitude'
+    url_template_kwargs : dict
+        **url_template_kwargs will be passed to url_template.format.
+        The latitude-longitude pairs will be stringified and added
+        under the key 'locations'.
+        Usually, this will just contain your google maps elevation API key
+        under the key 'api_key'.
+    decimal_places_to_round : int
+        round coorindates to some number of decimal places
+        to fit more locations in each API call
+    max_locations_per_batch : int
+        max number of coordinate pairs to submit in each API call (if this is
+        too high, the server will reject the request because its character
+        limit exceeds the max)
+    pause_duration : float
+        time to pause between API calls
+    url_template : string
+        URL to query for elevation data.
+        Unless you have an unusual network setup, let this default to
+        the standard Google Maps elevation API endpoint.
+    proxies : dict
+        proxies parameter for requests.get;
+        see `the requests documentation <https://2.python-requests.org//en/master/user/advanced/#proxies>`_.
+
+    Returns
+    -------
+    latitude_longitude_pairs : pd.DataFrame
+    """
+    log('Requesting node elevations from the API in {} calls.'.format(math.ceil(len(latitude_longitude_pairs) / max_locations_per_batch)))
+    # break the series of coordinates into chunks of size max_locations_per_batch
+    # API format is locations=lat,lng|lat,lng|lat,lng|lat,lng...
+    latlng_pair_format_string = '{:.' + str(decimal_places_to_round) + 'f},{:.' + str(decimal_places_to_round) + 'f}'
+    results = []
+    for i in range(0, len(latitude_longitude_pairs), max_locations_per_batch):
+        chunk = latitude_longitude_pairs.iloc[i : i + max_locations_per_batch]
+        locations = '|'.join(latlng_pair_format_string.format(row.latitude, row.longitude) for row in chunk.itertuples())
+        url = url_template.format(locations, **url_template_kwargs)
+
+        # check if this request is already in the cache (if global use_cache=True)
+        cached_response_json = get_from_cache(url)
+        if cached_response_json is not None:
+            response_json = cached_response_json
+        else:
+            try:
+                # request the elevations from the API
+                log('Requesting node elevations: {}'.format(url))
+                time.sleep(pause_duration)
+                response = requests.get(url, proxies=proxies)
+                response_json = response.json()
+                save_to_cache(url, response_json)
+            except Exception as e:
+                log(e)
+                log('Server responded with {}: {}'.format(response.status_code, response.reason))
+
+        # append these elevation results to the list of all results
+        results.extend(response_json['results'])
+
+    # sanity check that all our vectors have the same number of elements
+    if not (len(results) == len(latitude_longitude_pairs)):
+        raise Exception('We requested {} elevations but we received {} results from the elevation API.'.format(len(latitude_longitude_pairs), len(results)))
+    else:
+        log('We requested {} elevations and we received {} results from the elevation API.'.format(len(latitude_longitude_pairs), len(results)))
+
+    # add elevation as an attribute to the DataFrame
+    latitude_longitude_pairs['elevation'] = [result['elevation'] for result in results]
+    return latitude_longitude_pairs
 
 def add_node_elevations(G, api_key,
                         max_locations_per_batch=350,
@@ -90,10 +174,17 @@ def add_node_elevations(G, api_key,
     else:
         log('Graph has {} nodes and we received {} results from the elevation API.'.format(len(G.nodes()), len(results)))
 
-    # add elevation as an attribute to the nodes
     df = pd.DataFrame(node_points, columns=['node_points'])
     df['elevation'] = [result['elevation'] for result in results]
+
+    # sanity check that all our vectors have the same number of elements
+    if not (len(df['elevation']) == len(G.nodes())):
+        raise Exception('Graph has {} nodes but we received {} results from the elevation API.'.format(len(G.nodes()), len(df['elevation'])))
+    else:
+        log('Graph has {} nodes and we received {} results from the elevation API.'.format(len(G.nodes()), len(df['elevation'])))
+
     df['elevation'] = df['elevation'].round(3) # round to millimeter
+    # add elevation as an attribute to the nodes
     nx.set_node_attributes(G, name='elevation', values=df['elevation'].to_dict())
     log('Added elevation data to all nodes.')
 
