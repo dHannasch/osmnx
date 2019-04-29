8f0cc95ca80cb3b0bf0e0322ec0771033fb127b1
David A. Hannasch <David.A.Hannasch@gmail.com>
2019-04-29 12:49:52 -0600

    Create function get_nearest_edge_distances_kdtree and use that in get_nearest_edges, discarding the distances if we do not want them.

--- osmnx/utils.py
+++ osmnx/utils.py
@@ -628,6 +628,77 @@ def get_nearest_nodes(G, X, Y, method=None):
     return np.array(nn)
 
 
+def get_nearest_edge_distances_kdtree(G, X, Y, dist=0.0001):
+    """
+    Return the graph edges nearest to a list of points. Pass in points
+    as separate vectors of X and Y coordinates. The 'kdtree' method
+    is by far the fastest with large data sets, but only finds approximate
+    nearest edges if working in unprojected coordinates like lat-lng (it
+    precisely finds the nearest edge if working in projected coordinates).
+
+    Parameters
+    ----------
+    G : networkx multidigraph
+    X : list-like
+        The vector of longitudes or x's for which we will find the nearest
+        edge in the graph. For projected graphs, use the projected coordinates,
+        usually in meters.
+    Y : list-like
+        The vector of latitudes or y's for which we will find the nearest
+        edge in the graph. For projected graphs, use the projected coordinates,
+        usually in meters.
+
+    dist : float
+        spacing length along edges. Units are the same as the geom; Degrees for
+        unprojected geometries and meters for projected geometries. The smaller
+        the value, the more points are created.
+
+    Returns
+    -------
+    ne : GeoDataFrame
+        array of nearest edges represented by their startpoint and endpoint OSM
+        node IDs (columns 'u' and 'v'), and the distances (column 'distance').
+
+    Info
+    ----
+    The method creates equally distanced points along the edges of the network.
+    Then, these points are used in a kdTree search to identify which
+    is nearest. Note that this method will not give the exact perpendicular point
+    along the edge, but the smaller the *dist* parameter, the closer the solution
+    will be.
+
+    Code is adapted from an answer by JHuw from this original question:
+    https://gis.stackexchange.com/questions/222315/geopandas-find-nearest-point
+    -in-other-dataframe
+    """
+    # check if we were able to import scipy.spatial.cKDTree successfully
+    if not cKDTree:
+        raise ImportError('The scipy package must be installed to use this optional feature.')
+
+    # transform graph into DataFrame
+    edges = graph_to_gdfs(G, nodes=False, fill_edge_geometry=True)
+
+    # transform edges into evenly spaced points
+    edges['points'] = edges.apply(lambda x: redistribute_vertices(x.geometry, dist), axis=1)
+
+    # develop edges data for each created points
+    extended = edges['points'].apply([pd.Series]).stack().reset_index(level=1, drop=True).join(edges).reset_index()
+
+    # Prepare btree arrays
+    nbdata = np.array(list(zip(extended['Series'].apply(lambda x: x.x),
+                               extended['Series'].apply(lambda x: x.y))))
+
+    # build a k-d tree for euclidean nearest node search
+    btree = cKDTree(data=nbdata, compact_nodes=True, balanced_tree=True)
+
+    # query the tree for nearest node to each point
+    points = np.array([X, Y]).T
+    distances, idx = btree.query(points, k=1)  # Returns ids of closest point
+    eidx = extended.loc[idx, 'index']
+    ne = edges.loc[eidx, ['u', 'v']]
+    ne['distance'] = distances
+    return ne
+
 def get_nearest_edges(G, X, Y, method=None, dist=0.0001):
     """
     Return the graph edges nearest to a list of points. Pass in points
@@ -688,32 +759,7 @@ def get_nearest_edges(G, X, Y, method=None, dist=0.0001):
         ne = [(u, v) for _, u, v in ne]
 
     elif method == 'kdtree':
-
-        # check if we were able to import scipy.spatial.cKDTree successfully
-        if not cKDTree:
-            raise ImportError('The scipy package must be installed to use this optional feature.')
-
-        # transform graph into DataFrame
-        edges = graph_to_gdfs(G, nodes=False, fill_edge_geometry=True)
-
-        # transform edges into evenly spaced points
-        edges['points'] = edges.apply(lambda x: redistribute_vertices(x.geometry, dist), axis=1)
-
-        # develop edges data for each created points
-        extended = edges['points'].apply([pd.Series]).stack().reset_index(level=1, drop=True).join(edges).reset_index()
-
-        # Prepare btree arrays
-        nbdata = np.array(list(zip(extended['Series'].apply(lambda x: x.x),
-                                   extended['Series'].apply(lambda x: x.y))))
-
-        # build a k-d tree for euclidean nearest node search
-        btree = cKDTree(data=nbdata, compact_nodes=True, balanced_tree=True)
-
-        # query the tree for nearest node to each point
-        points = np.array([X, Y]).T
-        dist, idx = btree.query(points, k=1)  # Returns ids of closest point
-        eidx = extended.loc[idx, 'index']
-        ne = edges.loc[eidx, ['u', 'v']]
+        ne = get_nearest_edge_distances_kdtree(G, X, Y, dist=dist)[['u', 'v']]
 
     elif method == 'balltree':
 
